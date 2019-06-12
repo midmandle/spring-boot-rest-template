66be56c4b290e80e87965a93cf89409122014fc7
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-12 15:11:20 +0100

    Add test for Greeting controller

--- src/test/java/GreetingShould.java
+++ src/test/java/GreetingShould.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
 
 public class GreetingShould {
+    @Test
+    void return_200_OK() {
+        get("/greeting").then().assertThat().statusCode(200);
+    }
 }
